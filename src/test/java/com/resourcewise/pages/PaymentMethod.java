b6c852c615a5f6a46897f3d12ebc13e1bdf40f2a
prexa89 <prexa82@gmail.com>
2022-06-04 21:20:20 +0100

    new_branch2

---
+++ src/test/java/com/resourcewise/pages/PaymentMethod.java
@@ -0,0 +1,48 @@
+package com.resourcewise.pages;
+
+import com.resourcewise.driver.DriverManager;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class PaymentMethod extends DriverManager {
+    DriverManager driverManage=new DriverManager();
+
+   // @FindBy(id = "paymentmethod_1")
+   // WebElement paymentMethod;
+  //  @FindBy(xpath = "//button[@onclick='PaymentMethod.save()']")
+   // WebElement paymentMethodContinue;
+   //@FindBy(id = "CardholderName")
+   // WebElement cardholderName;
+   // @FindBy(id = "CardNumber")
+   // WebElement cardNumber;
+   // @FindBy(id ="ExpireMonth")
+   // WebElement expireMonth;
+   // @FindBy(id = "ExpireYear")
+  //  WebElement expireYear;
+   // @FindBy(id = "CardCode")
+   // WebElement cardCode;
+   // @FindBy(xpath = "//button[@onclick='PaymentInfo.save()'")
+  //  WebElement continueButtonPaymentInfo;
+  //  @FindBy(xpath = "//button[@onclick='ConfirmOrder.save()")
+  //  WebElement confirmOrderButton;
+    //public void paymentMethodOption(){
+      //  paymentMethod.click();
+   // }public void setPaymentMethodContinueBtn(){
+       // paymentMethodContinue.click();
+   // }public void cardholderNam(){
+       // cardholderName.sendKeys();
+  //  }public void cardNum(){
+        //cardNumber.sendKeys();
+   // }public void cardExpireMonth(){
+        //expireMonth.sendKeys();
+    //}public void cardExpireYear(){
+  //      expireYear.sendKeys();
+    //}public void codeOfCard(){
+       // cardCode.sendKeys();
+    //}public void contiBtnPaymentInfo(){
+       // continueButtonPaymentInfo.click();
+  //  }public void confirmOrderBtn(){
+       // confirmOrderButton.click();
+   // }
+
+}
---
+++ src/test/resources/Payment.feature
@@ -0,0 +1,8 @@
+
+Feature:
+
+  Scenario: payment functionality
+    Given I choose credit card for payment
+    And   I click on continue btn
+
+
